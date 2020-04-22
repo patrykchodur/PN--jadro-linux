2ba6028e1ba4e736afcf4012a61df3aad148b09f
Patryk Chodur <patrykchodur@icloud.com>
2020-04-22 23:41:11 +0200

    run_chodur_patryk.sh if inside repo go up

--- run_chodur_patryk.sh
+++ run_chodur_patryk.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# this script should only work above cloned repositories
+if [ -d .git ]; then
+	if [ -d ../run_chodur_patryk.sh ]; then
+		cp run_chodur_patryk.sh ..
+	fi
+	cd ..
+	echo "Info: this script is meant to use above cloned directory"
+fi
+
 if [ $1 = "clone" ]; then
 	if [ -d .git ]; then
 		echo "Error: already inside repository"
